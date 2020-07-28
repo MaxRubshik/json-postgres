f204b36aeb5279e276ae80dc1b80b5445ff19334
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-07-29 01:01:00 +0300

    finished

--- json-postgres/src/main/java/com/krlch/jsonpostgres/controllers/PersonController.java
+++ json-postgres/src/main/java/com/krlch/jsonpostgres/controllers/PersonController.java
@@ -1,15 +1,22 @@
 package com.krlch.jsonpostgres.controllers;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.krlch.jsonpostgres.entities.Person;
 import com.krlch.jsonpostgres.repositories.PersonRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 
 @RestController
 public class PersonController {
@@ -25,11 +32,30 @@ public class PersonController {
 
     @RequestMapping("json")
     public void json() {
-        URL url = this.getClass().getClassLoader().getResource("people.json");
+//        URL url = this.getClass().getClassLoader().getResource("people.json");
+//
+//        if (url != null) {
+        File jsonFile = null;
 
-        if (url != null) {
-            File jsonFile = new File(url.getFile());
-
+        try {
+            jsonFile = ResourceUtils.getFile("classpath:people.json");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
+
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            List<Person> people = objectMapper.readValue(jsonFile, new TypeReference<List<Person>>() {
+            });
+
+            personRepository.saveAll(people);
+            logger.info("All records saved.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        } else {
+//            logger.warn("URL is null.");
+//        }
     }
 }
